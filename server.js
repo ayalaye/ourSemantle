fbf3709149756a3989a5b7a971a8ff2081474377
Ayala Yerushalmi <asyerushalmi@gmail.com>
2023-09-12 22:01:39 +0300

    test6

--- server.js
+++ server.js
@@ -333,7 +333,7 @@ server.listen(my_port, () => {
 });
 
 // Create a task to execute at 01:00 (01:00 AM) every day
-const job = schedule.scheduleJob('0 19 * * *', function () {
+const job = schedule.scheduleJob('24 22 * * *', function () {
   startNewGame()
 });
 const currentTime = new Date();
