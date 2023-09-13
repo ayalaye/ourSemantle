531a3de79965a4e8a801404a925f11e24e368436
Ayala Yerushalmi <asyerushalmi@gmail.com>
2023-09-13 16:16:03 +0300

    teeessst

--- similarity.py
+++ similarity.py
@@ -1,21 +1,24 @@
 
-import sys
-import spacy
+# import sys
+# import spacy
 
-nlp = spacy.load('en_core_web_md')
+# nlp = spacy.load('en_core_web_md')
 
 #Calculates semantic proximity between 2 words
-def calculate_similarity(word1, word2):
-    token1 = nlp(word1)[0]
-    token2 = nlp(word2)[0]
+# def calculate_similarity(word1, word2):
+#     token1 = nlp(word1)[0]
+#     token2 = nlp(word2)[0]
    
-    similarity = token1.similarity(token2)
-    similarity *= 100 # Similiraty in percentages
-    return round(similarity, 2) # Round up to 2 digits after the decimal point
+#     similarity = token1.similarity(token2)
+#     similarity *= 100 # Similiraty in percentages
+#     return round(similarity, 2) # Round up to 2 digits after the decimal point
 
 if __name__ == "__main__":
-    word1 = sys.argv[1]
-    word2 = sys.argv[2]
+    # word1 = sys.argv[1]
+    # word2 = sys.argv[2]
    
-    similarity = calculate_similarity(word1, word2)
-    print(similarity)
+    # similarity = calculate_similarity(word1, word2)
+    # print(similarity)
+    print(3)
+
+
